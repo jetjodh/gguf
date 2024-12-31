8cf01c4a863a18f00d2359941040e4533a448d33
calcuis <113646141+calcuis@users.noreply.github.com>
2024-12-30 23:01:38 -0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+try:
+    import comfy.utils
+except ImportError:
+    pass
+else:
+    from .nodes import NODE_CLASS_MAPPINGS
+    NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
+    __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
