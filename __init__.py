bd5844eb4300a5e0ac0164a3862c43996ba2d208
calcuis <113646141+calcuis@users.noreply.github.com>
2025-01-13 21:19:54 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     pass
 else:
-    from .nodes import NODE_CLASS_MAPPINGS
+    from .node import NODE_CLASS_MAPPINGS
     NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
     __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
