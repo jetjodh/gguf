08f62063da3b90759ba02c01192b722a6029931f
calcuis <113646141+calcuis@users.noreply.github.com>
2025-02-19 15:56:16 -0800

    Update pig.py

--- pig.py
+++ pig.py
@@ -402,29 +402,86 @@ def load_gguf_clip(path):
         pass
     return sd
 class VaeGGUF:
+    @staticmethod
+    def vae_list():
+        vaes = folder_paths.get_filename_list("vae")
+        approx_vaes = folder_paths.get_filename_list("vae_approx")
+        sdxl_taesd_enc = False
+        sdxl_taesd_dec = False
+        sd1_taesd_enc = False
+        sd1_taesd_dec = False
+        sd3_taesd_enc = False
+        sd3_taesd_dec = False
+        f1_taesd_enc = False
+        f1_taesd_dec = False
+        for v in approx_vaes:
+            if v.startswith("taesd_decoder."):
+                sd1_taesd_dec = True
+            elif v.startswith("taesd_encoder."):
+                sd1_taesd_enc = True
+            elif v.startswith("taesdxl_decoder."):
+                sdxl_taesd_dec = True
+            elif v.startswith("taesdxl_encoder."):
+                sdxl_taesd_enc = True
+            elif v.startswith("taesd3_decoder."):
+                sd3_taesd_dec = True
+            elif v.startswith("taesd3_encoder."):
+                sd3_taesd_enc = True
+            elif v.startswith("taef1_encoder."):
+                f1_taesd_dec = True
+            elif v.startswith("taef1_decoder."):
+                f1_taesd_enc = True
+        if sd1_taesd_dec and sd1_taesd_enc:
+            vaes.append("taesd")
+        if sdxl_taesd_dec and sdxl_taesd_enc:
+            vaes.append("taesdxl")
+        if sd3_taesd_dec and sd3_taesd_enc:
+            vaes.append("taesd3")
+        if f1_taesd_dec and f1_taesd_enc:
+            vaes.append("taef1")
+        return vaes
+    @staticmethod
+    def load_taesd(name):
+        sd = {}
+        approx_vaes = folder_paths.get_filename_list("vae_approx")
+        encoder = next(filter(lambda a: a.startswith("{}_encoder.".format(name)), approx_vaes))
+        decoder = next(filter(lambda a: a.startswith("{}_decoder.".format(name)), approx_vaes))
+        enc = comfy.utils.load_torch_file(folder_paths.get_full_path_or_raise("vae_approx", encoder))
+        for k in enc:
+            sd["taesd_encoder.{}".format(k)] = enc[k]
+        dec = comfy.utils.load_torch_file(folder_paths.get_full_path_or_raise("vae_approx", decoder))
+        for k in dec:
+            sd["taesd_decoder.{}".format(k)] = dec[k]
+        if name == "taesd":
+            sd["vae_scale"] = torch.tensor(0.18215)
+            sd["vae_shift"] = torch.tensor(0.0)
+        elif name == "taesdxl":
+            sd["vae_scale"] = torch.tensor(0.13025)
+            sd["vae_shift"] = torch.tensor(0.0)
+        elif name == "taesd3":
+            sd["vae_scale"] = torch.tensor(1.5305)
+            sd["vae_shift"] = torch.tensor(0.0609)
+        elif name == "taef1":
+            sd["vae_scale"] = torch.tensor(0.3611)
+            sd["vae_shift"] = torch.tensor(0.1159)
+        return sd
     @classmethod
     def INPUT_TYPES(s):
-        gguf_names = [x for x in folder_paths.get_filename_list('model_gguf')]
-        return {'required': {'gguf_name': (gguf_names,)}}
-    RETURN_TYPES = 'VAE',
-    FUNCTION = 'load_vae'
-    CATEGORY = 'gguf'
+        return {"required": { "vae_name": (s.vae_list(), )}}
+    RETURN_TYPES = ("VAE",)
+    FUNCTION = "load_vae"
+    CATEGORY = "gguf"
     TITLE = 'GGUF VAE (Experimental)'
-    # @classmethod
-    # def get_filename_list(s):
-    #     files = []
-    #     files += folder_paths.get_filename_list('vae')
-    #     files += folder_paths.get_filename_list('vae_gguf')
-    #     return sorted(files)
-    def load_vae(self, gguf_name):
-        vae = []
-        for p in gguf_name:
-            if p.endswith('.gguf'):
-                sd = load_gguf_sd(p) # load_gguf_clip(p)
-            else:
-                sd = comfy.utils.load_torch_file(p)
-            vae = comfy.sd.VAE(sd=sd)
-        return vae
+    def load_vae(self, vae_name):
+        if vae_name.endswith('.gguf'):
+            sd = load_gguf_sd(vae_name)
+        if vae_name in ["taesd", "taesdxl", "taesd3", "taef1"]:
+            sd = self.load_taesd(vae_name)
+        else:
+            vae_path = folder_paths.get_full_path_or_raise("vae", vae_name)
+            sd = comfy.utils.load_torch_file(vae_path)
+        vae = comfy.sd.VAE(sd=sd)
+        return (vae,)
 class LoaderGGUF:
     @classmethod
     def INPUT_TYPES(s):
