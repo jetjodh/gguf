ebd607c7b36c29ffb912df5121ba1ffa7da1590e
calcuis <113646141+calcuis@users.noreply.github.com>
2025-03-07 22:16:41 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,3 +6,8 @@ else:
     from .pig import NODE_CLASS_MAPPINGS
     NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
     __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+from .gguf_connector.reader import *
+from .gguf_connector.writer import GGUFWriter, GGMLQuantizationType
+from .gguf_connector.const import GGML_QUANT_VERSION, LlamaFileType
+from .gguf_connector.quant import quantize, dequantize, QuantError
